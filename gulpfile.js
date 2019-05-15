521295ee09f6fc88c574737446494cc3dbf7168b
roberto0412 <roberto_esquivel04@outlook.com>
2019-05-14 19:13:06 -0700

    updated

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+// This is a very basic Gulp task,
+// with a name and some code to run
+// when this task is called:
+gulp.task("default", function(done) {
+  console.log("Hello world");
+  done();
+});
