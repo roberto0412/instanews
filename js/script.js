81d5ee7c41163c0d3c7a00026d3460b09b833d76
roberto0412 <roberto_esquivel04@outlook.com>
2019-06-10 19:23:33 -0700

    Working Grid with media queries, and loader .gif functionality.

--- js/script.js
+++ js/script.js
@@ -9,19 +9,38 @@ $(function() {
         section +
         ".json?api-key=TGUafZr6GLIQMSeuKro7lIaTPAUq4U5c"
     }).done(function(data) {
+      let cont = 0;
       $(".lines").html("");
       $.each(data.results, function(key, value) {
         console.log(data.results);
-        try {
-          $(".lines").append(`
+        if (data.results[key].multimedia.length >= 5 && cont < 12) {
+          console.log("hellow");
+          try {
+            cont = cont + 1;
+            $(".lines").append(`
             <li><a href= '${
               data.results[key].url
-            }'><div class="images" style="background-image:url('${data.results[key].multimedia[4].url}')"><div class="abstract"> <p class=ptop>'${data.results[key].abstract}'</p></div> </div></a></li>
+            }'><div class="images" style="background-image:url('${
+              data.results[key].multimedia[4].url
+            }')"><div class="abstract"> <p class=ptop>${
+              data.results[key].abstract
+            }</p></div> </div></a></li>
         `);
-        } catch (error) {
-          console.log(error);
+          } catch (error) {
+            console.log(error);
+          }
         }
       });
     });
   });
 });
+
+const $loading = $(".loader");
+$loading.hide();
+$(document)
+  .ajaxStart(function() {
+    $loading.show();
+  })
+  .ajaxStop(function() {
+    $loading.hide();
+  });
