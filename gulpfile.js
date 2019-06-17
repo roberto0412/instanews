70857dfc00afeadc34cb51374c72df95f05825aa
roberto0412 <roberto_esquivel04@outlook.com>
2019-06-16 23:37:41 -0700

    Las commit, No coments , all Updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,28 @@ const gulp = require("gulp");
 const watch = require("gulp-watch");
 const eslint = require("gulp-eslint");
 const sass = require("gulp-sass");
-// Load Gulp!
-// Now that we've installed the terser package we can require it:
 const terser = require("gulp-terser"),
   rename = require("gulp-rename");
 
-gulp.task("scripts", function() {
+gulp.task("scripts", function () {
   return gulp
-    .src("./js/*.js") // What files do we want gulp to consume?
+    .src("./js/*.js")
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
-    .pipe(terser()) // Call the terser function on these files
-    .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
-    .pipe(gulp.dest("./build/js")); // Where do we put the result?
+    .pipe(terser())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task("sass", function() {
+gulp.task("sass", function () {
   return gulp
     .src("./sass/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./build/css"));
 });
 
-gulp.task("watch", function(done) {
+gulp.task("watch", function (done) {
   gulp.watch("./js/*.js", gulp.series("scripts"));
   gulp.watch("./sass/*.scss", gulp.series("sass"));
   done();
--- js/script.js
+++ js/script.js
@@ -1,46 +1,55 @@
-$(function() {
-  $("select").on("change", function() {
+$(function () {
+  $('select').on('change', function () {
+    $('header').addClass('header2');
+
     const section = $(this).val();
-    // console.log(section);
-    $.ajax({
-      method: "GET",
-      url:
-        "https://api.nytimes.com/svc/topstories/v2/" +
-        section +
-        ".json?api-key=TGUafZr6GLIQMSeuKro7lIaTPAUq4U5c"
-    }).done(function(data) {
-      let cont = 0;
-      $(".lines").html("");
-      $.each(data.results, function(key, value) {
-        console.log(data.results);
-        if (data.results[key].multimedia.length >= 5 && cont < 12) {
-          console.log("hellow");
-          try {
-            cont = cont + 1;
-            $(".lines").append(`
+    if (section === '') {
+      $('.lines').empty();
+    } else {
+
+      $.ajax({
+        method: 'GET',
+        url:
+          'https://api.nytimes.com/svc/topstories/v2/' +
+          section +
+          '.json?api-key=TGUafZr6GLIQMSeuKro7lIaTPAUq4U5c'
+      }).done(function (data) {
+        let cont = 0;
+        $('.lines').html('');
+        $.each(data.results, function (key) {
+
+          if (data.results[key].multimedia.length >= 5 && cont < 12) {
+
+            try {
+              cont = cont + 1;
+              $('.lines').append(`
             <li><a href= '${
-              data.results[key].url
-            }'><div class="images" style="background-image:url('${
-              data.results[key].multimedia[4].url
-            }')"><div class="abstract"> <p class=ptop>${
-              data.results[key].abstract
-            }</p></div> </div></a></li>
+                data.results[key].url
+                }'><div class="images" style="background-image:url('${
+                data.results[key].multimedia[4].url
+                }')"><div class="abstract"> <p class="ptop">${
+                data.results[key].abstract
+                }</p></div> </div></a></li>
         `);
-          } catch (error) {
-            console.log(error);
+            } catch (error) {
+
+              $('.lines').html(error);
+
+            }
+
           }
-        }
+        });
       });
-    });
+    }
   });
 });
 
-const $loading = $(".loader");
+const $loading = $('.loader');
 $loading.hide();
 $(document)
-  .ajaxStart(function() {
+  .ajaxStart(function () {
     $loading.show();
   })
-  .ajaxStop(function() {
+  .ajaxStop(function () {
     $loading.hide();
   });
